3bd8be309010b152f2a8a9e3a0f35521f1e781d6
Ricardo Sobral <37637218+RicardoSobral-7@users.noreply.github.com>
2020-07-23 22:04:44 -0300

    otimization, not-found page

--- desafio4/routes.js
+++ desafio4/routes.js
@@ -1,34 +1,12 @@
 const express = require("express");
-const courses = require("./data")
 const routes = express.Router();
-const teachers = require("./teachers")
+const teachers = require("./teachers");
 
-
-
-routes.get("/", (req,res)=>{
-    return res.render("about");
+routes.get("/",(req,res)=>{
+    return res.redirect("/teachers")
 });
 
-routes.get("/admin",(req,res)=>{
-    return res.redirect("/teacher")
-});
-
-routes.get("/content",(req,res)=>{
-    return res.render("content",{coursesCard: courses});
-});
-
-routes.get("/course/:id", (req,res)=>{
-    const id = req.params.id;
-    const course = courses.find((course)=>{
-        return course.id == id;
-    });
-    if(!course){
-        return res.send("Course not found!")
-    }
-    return res.render("course", {item: course});
-});
-
-routes.get("/teacher", (req,res)=>{
+routes.get("/teachers", (req,res)=>{
     return res.render("teachers/teacher");
 });
 
@@ -40,8 +18,12 @@ routes.get("/teacher/:id", teachers.show);
 
 routes.get("/teacher/:id/edit", teachers.edit);
 
-
 routes.post("/teacher", teachers.post)
+
+routes.put("/teacher", teachers.put);
+
+routes.delete("/teacher", teachers.delete);
+
 
 
 
--- desafio4/teachers.js
+++ desafio4/teachers.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const data = require("./data.json");
 const {age, graduation, date} = require("./utils");
 
-exports.show = (req,res) =>{
+exports.show = (req,res) => {
     const {id} = req.params;
 
     const foundTeacher = data.teachers.find(function(teacher){
@@ -66,4 +66,49 @@ exports.edit = (req,res) => {
 
 
     return res.render("teachers/edit",{teacher});
+}
+
+exports.put = (req,res) => {
+    const {id} = req.body;
+    let index = 0;
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex){
+        if(id == teacher.id){
+            index = foundIndex;
+            return true;
+        }
+    });
+    if(!foundTeacher){
+        return res.send("Teacher not found!");
+    }
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+    data.teachers[index] = teacher;
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err){
+            return res.send("Write error detected, contact the suport");
+        }
+        return res.redirect(`/teacher/${id}`);
+    });
+}
+
+exports.delete = (req,res) =>{
+    const {id} = req.body;
+
+    const filteredTeachers = data.teachers.filter((teacher)=>{
+        return teacher.id != id;
+    });
+    data.teachers = filteredTeachers;
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err){
+        if(err){
+            return res.send("Write file error!");
+        }
+        return res.redirect("/teacher");
+        
+    })
 }
